046b29028bbab4408b324ef116e3c884e3b2a1f1
Ashu8408 <92422960+Ashu8408@users.noreply.github.com>
2024-08-10 23:23:52 +0530

    Update day1-hello.rb

--- day1-hello.rb
+++ day1-hello.rb
@@ -1,1 +1,36 @@
-y
+puts "check hello, yes working"
+a=5.5
+b=2.25
+c=a+b
+puts a+b
+puts (a/b)
+puts (a%b)
+puts ("the sum of a + b = c")
+puts "the sum of #{a} + #{b} = #{c}"
+puts a-3;
+
+print "hello print check"
+puts "1st line"
+puts "2nd line"
+print "3rd line"
+print "4th line\n"
+print "5th line\n\n"
+
+def add(a,b)
+    
+    puts "add function called"
+    c=a+b
+    return (c)
+end
+
+puts (add(2,3))
+puts "#{(add(4.3, 8))} ---adding with text in puts"
+
+puts a,b,i=32234,j=34312,c,add(6,9)
+print a, "," ,c, "\n", "#{a},#{b}"
+
+x,y,z=9,8,2
+
+print "\n #{x}, #{y}, #{z}"
+print x,y,z
+
